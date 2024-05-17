c774a48615422f5892fe163b9dfac5c4e063132f
mrkhish <mrkhish@gmail.com>
2024-05-17 16:29:10 +0330

    lastest filament changes

---
+++ vendor/ralphjsmit/laravel-seo/src/Support/LinkTag.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace RalphJSmit\Laravel\SEO\Support;
+
+class LinkTag extends Tag
+{
+    public string $tag = 'link';
+
+    public function __construct(
+        public string $rel,
+        public string $href,
+    ) {
+    }
+}
---
+++ vendor/ralphjsmit/laravel-seo/src/Support/MetaContentTag.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace RalphJSmit\Laravel\SEO\Support;
+
+class MetaContentTag extends Tag
+{
+    public string $tag = 'meta';
+
+    public function __construct(
+        public string $property,
+        public string $content,
+    ) {
+    }
+}
